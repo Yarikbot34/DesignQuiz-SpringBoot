5561e80ec42645770819e689603ded194e128e9f
Kozinak <yroslavbot1@gmail.com>
2026-04-28 21:10:34 +0300

    Write order to orders list

--- src/main/java/main/sbdesignquiz/controllers/quizRouter.java
+++ src/main/java/main/sbdesignquiz/controllers/quizRouter.java
@@ -24,16 +24,6 @@ public class quizRouter {
 
     @PostMapping("/quiz/result")
     public String handlePost(@RequestBody Order order) {
-        System.out.println("=== Получен POST-запрос ===");
-        System.out.println("Имя: " + order.getName());
-        System.out.println("Телефон: " + order.getPhone());
-        System.out.println("Email: " + order.getEmail());
-        System.out.println("Тип комнаты: " + order.getRoomType());
-        System.out.println("Площадь: " + order.getArea());
-        System.out.println("Зоны: " + Arrays.toString(order.getZones()));
-        System.out.println("Стиль: " + order.getStyle());
-        System.out.println("Бюджет: " + order.getBudget());
-        System.out.println("Комментарий: " + order.getComment());
         return "OK";
 
     }
